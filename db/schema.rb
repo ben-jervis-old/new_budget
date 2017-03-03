e7f01819cf7dd2ee93d43f6e3270d9ebe597d6d3
Ben Jervis <ben@jervis.net.au>
2017-03-03 18:11:42 +1100

    User modelling complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303010247) do
+ActiveRecord::Schema.define(version: 20170303011924) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
