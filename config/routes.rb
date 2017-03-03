3386075cb29622cb573f9b54ef8193b5ed0421fb
Ben Jervis <ben@jervis.net.au>
2017-03-04 10:48:06 +1100

    User controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#home'
+  get 'users/new'
 
+  get 'users/show'
+
+  root  'static_pages#home'
+  get   '/signup', to: 'users#new'
+  resources :users, except: :index
 end
