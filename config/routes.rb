de20951b2661552b6da9ba77558596404d2117bc
Ben Jervis <ben@jervis.net.au>
2017-03-06 19:35:47 +1100

    Basic login complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-
-  get 'sessions/new'
-
-  get 'users/new'
-
-  get 'users/show'
-
-  root  'static_pages#home'
-  get   '/signup', to: 'users#new'
-  post  '/signup', to: 'users#create'
+  get     'users/show'
+  root    'static_pages#home'
+  get     '/signup', to: 'users#new'
+  post    '/signup', to: 'users#create'
+  get     '/login',  to: 'sessions#new'
+  post    '/login',  to: 'sessions#create'
+  delete  '/logout', to: 'sessions#destroy'
   resources :users, except: [:index, :new, :create]
 end
